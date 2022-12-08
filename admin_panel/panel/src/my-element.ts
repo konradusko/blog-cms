23737bcffebd3f1c33c7f0f3d28bc6698700b780
konradusko <boleslawski.konrad@gmail.com>
2022-12-08 21:36:24 +0100

    Little change with styles

--- admin_panel/panel/src/my-element.ts
+++ admin_panel/panel/src/my-element.ts
@@ -10,7 +10,81 @@ export class PanelElement extends LitElement {
 
   render() {
     return html`
+    <div class="md:fixed md:w-full md:top-0 md:z-20 flex flex-row flex-wrap items-center bg-white p-6 border-b border-gray-300  h-[5rem]">
+    
+    <!-- logo -->
+    <div class="flex-none w-56 flex flex-row items-center">
+      <img src="img/logo.png" class="w-10 flex-none">
+      <strong class="capitalize ml-1 flex-1">cleopatra</strong>
 
+      <button id="sliderBtn" class="flex-none text-right text-gray-900 hidden md:block">
+        <i class="fad fa-list-ul"></i>
+      </button>
+    </div>
+    <!-- end logo -->   
+    
+
+    <!-- navbar content -->
+    <div  class="animsated md:hidden md:fixed md:top-0 md:w-full md:left-0 md:mt-16 md:border-t md:border-b md:border-gray-200 md:p-10 md:bg-white flex-1 pl-3 flex flex-row flex-wrap justify-between items-center md:flex-col md:items-center">
+      <!-- left -->
+      <div class="text-gray-600 md:w-full md:flex md:flex-row md:justify-evenly md:pb-10 md:mb-10 md:border-b md:border-gray-200">
+             <!-- left -->
+      </div>
+      <!-- end left -->      
+
+      <!-- right -->
+      <div class="flex flex-row-reverse items-center"> 
+
+        <!-- right -->
+
+      </div>
+      <!-- end right -->
+    </div>
+    <!-- end navbar content -->
+
+  </div>
+<!-- end navbar -->
+
+
+<!-- strat wrapper -->
+<div  class="h-[calc(100vh_-_5rem)] flex flex-row flex-wrap">
+  
+    <!-- start sidebar -->
+  <div  class="relative flex flex-col flex-wrap bg-white border-r border-gray-300 p-6 flex-none w-64 md:-ml-64 md:fixed md:top-0 md:z-30 md:h-screen md:shadow-xl animated faster">
+    
+
+    <!-- sidebar content -->
+    <div class="flex flex-col">
+
+  
+      <p class="uppercase text-xs text-gray-600 mb-4 tracking-wider">homes</p>
+
+      <!-- link -->
+      <a href="./index.html" class="mb-3 capitalize font-medium text-sm hover:text-teal-600 transition ease-in-out duration-500">
+        <i class="fad fa-chart-pie text-xs mr-2"></i>                
+        test
+      </a>
+      <!-- end link -->
+
+      
+      
+
+    </div>
+    <!-- end sidebar content -->
+
+  </div>
+  <!-- end sidbar -->
+
+  <!-- strat content -->
+  <div class="bg-gray-100 flex-1 p-6 md:mt-16 "> 
+
+    
+
+
+  </div>
+  <!-- end content -->
+
+</div>
     `
   }
 
--- admin_panel/panel/tailwind.config.cjs
+++ admin_panel/panel/tailwind.config.cjs
@@ -3,10 +3,24 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
-    './node_modules/tw-elements/dist/js/**/*.{js,css,min.css}'
+    './node_modules/tw-elements/dist/js/**/*.{js,css,min.css}',
+    './node_modules/tailwindcss-animatecss/dist/js/**/*.{js,css,min.css}'
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': {'max': '639px'},
+
+      'md': {'max': '767px'},
+
+      'lg': {'max': '1023px'},
+
+      'xl': {'max': '1279px'},
+    },
+    extend: {   spacing: {
+      '72': '18rem',
+      '84': '21rem',
+      '96': '24rem',
+    },},
   },
   plugins: [
     require('tailwindcss'),
@@ -15,6 +29,7 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
-    require('tw-elements/dist/plugin')
+    require('tw-elements/dist/plugin'),
+ 
   ],
 }

9a557056fadfbe46bd869beae332719ad2a501a7
konradusko <boleslawski.konrad@gmail.com>
2022-12-14 17:05:14 +0100

    Working with smtp page

--- admin_panel/panel/src/pages/setting_page_options/smtp.ts
+++ admin_panel/panel/src/pages/setting_page_options/smtp.ts
@@ -10,6 +10,11 @@ interface Smtp_object  {
   role:string,
   user:string
 }
+enum Smtp_panel_status{
+  pending="pending",
+  sucess='sucess',
+  error='error'
+}
 interface Response_smtp{
   message:string,
   error:boolean,
@@ -26,7 +31,9 @@ export class DomainOption extends LitElement {
   @property()
   smtp_type:string = ``
   @property()
-  smtp_data:null = null
+  smtp_data:null|Smtp_object = null
+  @property()
+  smtp_status:Smtp_panel_status = Smtp_panel_status.pending
   changePage(e:any){
     const route:Pages_settings = Pages_settings.main
     const options = {
@@ -38,15 +45,10 @@ export class DomainOption extends LitElement {
 }
 
   get_smtp_data(){
-    setTimeout(()=>{
-      create_alert(Alert_types.error,5,"test1",this.shadowRoot as ShadowRoot)
-    },6000)
-    setTimeout(()=>{
-      create_alert(Alert_types.warning,10,"test2",this.shadowRoot as ShadowRoot)
-    },7000)
-    setTimeout(()=>{
-      create_alert(Alert_types.warning,5,"test3",this.shadowRoot as ShadowRoot)
-    },7000)
+    this.smtp_status= Smtp_panel_status.pending
+    for(let i =0;i<100;i++){
+      create_alert(Alert_types.error,60,"test1",this.shadowRoot as ShadowRoot)
+    }
     fetch(`/api/v1/get/smtp`, {
       method: 'POST',
       headers: {
@@ -61,9 +63,14 @@ export class DomainOption extends LitElement {
       console.log(res)
     })
     .catch((er)=>{
-      console.log(er,'blad')
+      create_alert(Alert_types.error,5,"Wystąpił błąd",this.shadowRoot as ShadowRoot)
+      setTimeout(() => {
+        this.smtp_status = Smtp_panel_status.error
+      }, 1000);
+   
     })
   }
+
   connectedCallback(): void {
     super.connectedCallback()
     this.get_smtp_data()
@@ -77,9 +84,75 @@ export class DomainOption extends LitElement {
     <div class="flex rounded-lg shadow-lg bg-white  w-[80%] h-[98%] flex-col">
       <div class="py-3 px-6 border-b border-gray-300 ">
       <h5 class="text-gray-900 text-xl font-medium mb-2 text-center">Ustawienia smtp dla ${this.smtp_type == Pages_settings.smtp_system_mail?`maili systemowych`:'maili newsletter'}</h5>
+      <nav class="rounded-md w-full">
+  <ol class="list-reset flex">
+    <li><button @click="${this.changePage}" class="text-blue-600 hover:text-blue-700">Ustawienia systemowe</button></li>
+    <li><span class="text-gray-500 mx-2">/</span></li>
+    <li class="text-gray-500">Smtp</li>
+  </ol>
+</nav>
       </div>
       <div class="p-6 h-[80%] max-h-[100%] overflow-y-auto">
  
+      ${this.smtp_status == Smtp_panel_status.pending?html`<div  class=" p-4 space-y-4 w-full rounded border border-gray-200 divide-y divide-gray-200 shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700">
+      <div class="flex justify-between items-center">
+          <div>
+              <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+      </div>
+      <div class="flex justify-between items-center pt-4">
+          <div>
+              <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+      </div>
+      <div class="flex justify-between items-center pt-4">
+          <div>
+              <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+      </div>
+      <div class="flex justify-between items-center pt-4">
+          <div>
+              <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+      </div>
+      <div class="flex justify-between items-center pt-4">
+          <div>
+              <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+          </div>
+          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+      </div>
+      <span class="sr-only">Loading...</span>
+  </div>`:html`
+    ${this.smtp_status == Smtp_panel_status.error?html`
+    <div  class="p-4 mb-4 border border-red-300 rounded-lg bg-red-50 dark:bg-red-200 animated fadeInDown" >
+  <div class="flex items-center">
+    <svg aria-hidden="true" class="w-5 h-5 mr-2 text-red-900 dark:text-red-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+    <span class="sr-only">Info</span>
+    <h3 class="text-lg font-medium text-red-900 dark:text-red-800">Wystąpił błąd</h3>
+  </div>
+  <div class="mt-2 mb-4 text-sm text-red-900 dark:text-red-800">
+    Wystąpił błąd podczas pobierania danych. Sprawdź połączenie z internetem i spróbuj ponownie.
+  </div>
+  <div class="flex">
+    <button @click="${this.get_smtp_data}" type="button" class="text-white bg-red-900 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-red-800 dark:hover:bg-red-900">
+    <svg class="-ml-0.5 mr-2 h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path></svg>
+      Spróbuj ponownie
+    </button>
+   
+  </div>
+</div>
+    `:html``}
+  `}
+
       
       <div class="relative z-0 mb-6 w-full group">
       <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />

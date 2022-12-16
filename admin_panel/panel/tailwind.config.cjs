e26cc79871a0418442783ba22905e5398ee20348
konradusko <boleslawski.konrad@gmail.com>
2022-12-16 19:00:34 +0100

    something gone wrong

--- admin_panel/panel/tailwind.config.cjs
+++ admin_panel/panel/tailwind.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/**/**.{vue,js,ts,jsx,tsx}",
     './node_modules/tw-elements/dist/js/**/*.{js,css,min.css}',
     './node_modules/tailwindcss-animatecss/dist/js/**/*.{js,css,min.css}'
   ],
